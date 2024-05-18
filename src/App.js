8794bb3e9111d067e8fa1e83e17aa8c02adada0c
Mario Basic <mbasic99@gmail.com>
2024-05-18 10:03:01 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import fer_logo from './assets/fer_logo.png';
+import unizg_logo from './assets/unizglogo.png';
+import fer_logo_long from './assets/fer_logo_long.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="header-flex-container">
+                    <img src={fer_logo} className="fer-logo" alt="logo" />
+                </div>
+            </header>
+            <main>
+                <div>
+                    UPIŠITE URL I PROVJERITE INTERNETSKU TRGOVINU
+                </div>
+
+                <form method="POST" id="url_form" autocomplete="off">
+                    <label for="url"></label>
+                    <input class="" type="text" id="url" maxlength="150" name="url" placeholder="https://www..."></input>
+                    <button type="submit" id="button" aria-label="Submit button"
+                        style={{}}>
+                        Submit
+                    </button>
+
+                </form>
+            </main>
+
+
+            <footer class="App-footer">
+                <div class="logos-flex-container">
+                    <a href="https://www.fer.unizg.hr/" target="_blank" rel="noreferrer">
+                        <img src={fer_logo_long} className="fer-logo-long" alt="logo" />
+                    </a>
+                    <a href="https://www.unizg.hr/" target="_blank" rel="noreferrer">
+                        <img src={unizg_logo} className="unizg-logo" alt="logo" />
+                    </a>
+                </div>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
